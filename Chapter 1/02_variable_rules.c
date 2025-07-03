8c4c973b89354a0a8c549b6ff79dbcb28f3ac164
tushar <iktushar2020@gmail.com>
2025-07-03 08:48:23 +0600

    chapter 1

---
+++ Chapter 1/02_variable_rules.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a; 
+    int df;
+    // int i am tushar;
+    // int 6;
+    return 0;
+}
---
+++ Chapter 1/03_dataTypes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    // int a;
+    // a = 1;
+
+    int a = 1;
+    // float b = 1.4;
+    float b;
+    b = 1.2342343;
+
+    char c = 'g';
+    char d;
+    d = 'h';
+
+
+    printf("The value of a is %d\n", a);
+    printf("The value of a is %f\n", b);
+    printf("The value of a is %c\n", c);
+    return 0;
+}
