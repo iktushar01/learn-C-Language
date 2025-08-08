4ab6dba284c8b0633f40de3cacd4425a63959be3
tushar <iktushar2020@gmail.com>
2025-08-08 10:34:38 +0600

    array learn

---
+++ Prectice for final exam/array/Example.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    // code here
+    int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+    float avg, sum = 0;
+    int i;
+
+    // Get the length of the array
+    int length = sizeof(ages) / sizeof(ages[0]);
+
+    // Loop through the elements of the array
+    for ( i = 0; i < length; i++){
+        sum = sum + ages[i];
+    }
+
+    // Calculate the average by dividing the sum by the length
+    avg = sum / length;
+
+    // Print the average
+    printf("The average age is: %.2f", avg);
+    return 0;
+}
---
+++ Prectice for final exam/array/array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // code here
+    int myNumbers[] = {25, 50, 75, 100};
+    myNumbers[2] = 1;
+    printf("%d", myNumbers[2]);
+    return 0;
+}
